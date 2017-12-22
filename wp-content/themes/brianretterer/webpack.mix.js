fe00d1be5f2e6a1a50cab80844a29a41cfb4a71a
Brian Retterer <bretterer@gmail.com>
2017-12-22 18:22:57 -0500

    reset

--- wp-content/themes/brianretterer/webpack.mix.js
+++ wp-content/themes/brianretterer/webpack.mix.js
@@ -1,14 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.sass('resources/assets/sass/app.scss', './css/main.css')
-    .sass('resources/assets/sass/default-color.scss', './css/color-default.css')
-    .sass('resources/assets/sass/orange-color.scss', './css/color-orange.css')
-    .options({
-        processCssUrls: false
-    })
-    .js('resources/assets/js/app.js', './js/app.js')
-    .copy('resources/assets/img', './img')
-    .copy('resources/assets/fonts', './fonts')
-    .copy('resources/assets/js/customizer-toggle-control.js', './js/customizer-toggle-control.js')
-    .copy('resources/assets/css/pure-css-togle-buttons.css', './css/pure-css-togle-buttons.css')
+mix.sass('assets/sass/app.scss', './css/main.css')
+    .js('assets/js/app.js', './js/app.js')
     .sourceMaps();
